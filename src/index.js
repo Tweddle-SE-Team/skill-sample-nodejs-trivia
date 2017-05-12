e497f8daa0c37d4c420dda1ae78193c223d18b1c
Joseph Horsch <jhorsch@tweddle.com>
2017-05-12 13:27:57 -0400

    Added start session.

--- src/index.js
+++ src/index.js
@@ -91,39 +91,8 @@ var newSessionHandlers = {
 var startSessionHandler = Alexa.CreateStateHandler(GAME_STATES.START_SESSION, {
   'StartSession': function(newGame) {
     var speechOutput = newGame ? this.t('NEW_GAME_MESSAGE', this.t('GAME_NAME')) + this.t('WELCOME_MESSAGE', GAME_LENGTH.toString()) : '';
-    // Select GAME_LENGTH questions for the game
-    var translatedQuestions = this.t('QUESTIONS');
-    var gameQuestions = populateGameQuestions(translatedQuestions);
-    // Generate a random index for the correct answer, from 0 to 3
-    var correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
-    // Select and shuffle the answers for each question
-    var roundAnswers = populateRoundAnswers(gameQuestions, 0, correctAnswerIndex, translatedQuestions);
-    var currentQuestionIndex = 0;
-    var spokenQuestion = Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0];
-    var repromptText = this.t('TELL_QUESTION_MESSAGE', '1', spokenQuestion);
 
-    // for (var i = 0; i < ANSWER_COUNT; i++) {
-    //   repromptText += (i + 1).toString() + '. ' + roundAnswers[i] + '. ';
-    // }
-    _.each(_.take(roundAnswers, ANSWER_COUNT), function(item, index) {
-      repromptText += (index + 1).toString() + '. ' + item + '. ';
-    });
-
-    speechOutput += repromptText;
-
-    Object.assign(this.attributes, {
-      'speechOutput': repromptText,
-      'repromptText': repromptText,
-      'currentQuestionIndex': currentQuestionIndex,
-      'correctAnswerIndex': correctAnswerIndex + 1,
-      'questions': gameQuestions,
-      'score': 0,
-      'correctAnswerText': translatedQuestions[gameQuestions[currentQuestionIndex]][Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0]][0]
-    });
-
-    // Set the current state to trivia mode. The skill will now use handlers defined in triviaStateHandlers
-    this.handler.state = GAME_STATES.TRIVIA;
-    this.emit(':askWithCard', speechOutput, repromptText, this.t('GAME_NAME'), repromptText);
+    this.emit(':askWithCard', 'You are about to start a new session.');
   }
 });
 
